4a74c94477b6803f24f89e4a31177352a671630e
Mark <git@happybeing.com>
2020-02-18 16:28:02 +0000

    Add external button to load alternate fixed data into Voyager

--- src/App.svelte
+++ src/App.svelte
@@ -3,11 +3,36 @@ import {onMount} from 'svelte';
 
 import {Voyager, CreateVoyager} from 'datavoyager';
 
+const fixedData1 = {
+  "values": [
+    {"fieldA": "A", "fieldB": 28}, {"fieldA": "B", "fieldB": 55}, {"fieldA": "C", "fieldB": 43},
+    {"fieldA": "D", "fieldB": 91}, {"fieldA": "E", "fieldB": 81}, {"fieldA": "F", "fieldB": 53},
+  ]
+};
+
+const fixedData2 = {
+  "values": [
+    {"fieldA": "A", "fieldB": 28}, {"fieldA": "B", "fieldB": 55}, {"fieldA": "C", "fieldB": 43},
+    {"fieldA": "G", "fieldB": 19}, {"fieldA": "H", "fieldB": 87}, {"fieldA": "I", "fieldB": 52}
+  ]
+};
+
+let voyagerInstance;
+let loadData;
+
+function onLoadFixedData () {
+  console.log('onLoadFixedData()');
+  if (voyagerInstance !== undefined) {
+  	loadData = loadData === fixedData1 ? fixedData2 : fixedData1;
+    voyagerInstance.updateData(loadData);
+  }
+}
+
 onMount(() => {
   const container = document.getElementById("voyager-embed");
   const config = undefined;
   const data = undefined;
-  const voyagerInstance = CreateVoyager(container, config, data);
+  voyagerInstance = CreateVoyager(container, config, data);
 });
 </script>
 
@@ -26,5 +51,6 @@ By theWebalyst.
 Code: <a href="https://github.com/theWebalyst/svelte-vega-voyager-embed">github</a> 
 Twitter: <a href="https://twitter.com/safepress">@safepress</a>
 </p>
+<p><button on:click={() => onLoadFixedData()}>Load Fixed Data</button></p>
 
 <div id='voyager-embed'></div>
